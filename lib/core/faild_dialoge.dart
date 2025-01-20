b38466f012bf3759c28fc95f70ce4b95f8ab22cd
Mohamed Zayan <zayanmohamed425@gmail.com>
2025-01-20 20:09:38 +0200

    some edite at logic at level screen

--- lib/core/faild_dialoge.dart
+++ lib/core/faild_dialoge.dart
@@ -1,7 +1,5 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:quizz/cubits/data_cubit/get_data_cubit.dart';
 
 faildDialoge(BuildContext context ,)
 {
@@ -11,7 +9,7 @@ faildDialoge(BuildContext context ,)
     headerAnimationLoop: false,
     animType: AnimType.scale,
     title: 'Error',
-    desc: 'Please fill in all required fields!',
+    desc: 'The answer isn\'t correct Please try again',
     btnCancelOnPress: () {
     },
   )..show();
--- lib/features/level/presentation/widgets/done_button.dart
+++ lib/features/level/presentation/widgets/done_button.dart
@@ -1,4 +1,3 @@
-import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
