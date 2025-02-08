db5ec7a63a2102af187fff1125da179cfc312b82
Mohamed Zayan <zayanmohamed425@gmail.com>
2025-02-08 20:31:56 +0200

    make userModel and add it to realtime database

--- lib/core/faild_dialoge.dart
+++ lib/core/faild_dialoge.dart
@@ -1,8 +1,6 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../cubits/answer_cubit/answer_cubit.dart';
 
 
 faildDialoge(BuildContext context ,List<String>answers)
